a684d37257b7cea5d26ff0e1b7fa98c2df7a8386
Fireblaze3127 <chris311327@gmail.com>
2020-04-06 07:32:34 -0400

    finished sign-in page

--- index.php
+++ index.php
@@ -6,51 +6,53 @@
 </head>
 
 	<body>
-	<img src = "images/Amazon logo.png"/>
-	<h1>Sign in to Amazon DSS</h1>
+		<img src = "images/Amazon logo.png"/>
+		<h1>Sign in to Amazon DSS</h1>
 
-	<form action="comparison.php" method="GET">
-		
-		<?php
-			// Enable error logging: 
-			error_reporting(E_ALL ^ E_NOTICE);
-			// mysqli connection via user-defined function
-			include ('./my_connect.php');
-			$mysqli = get_mysqli_conn();
-		?>
-
-		<?php
-			// SQL statement
-			$sql = "SELECT c.customerID, c.customerName "
-				. "FROM customer c";
+		<div class="login">
+			<form action="comparison.php" method="GET">
 				
-			// Prepared statement, stage 1: prepare
-			$stmt = $mysqli->prepare($sql);
+				<?php
+					// Enable error logging: 
+					error_reporting(E_ALL ^ E_NOTICE);
+					// mysqli connection via user-defined function
+					include ('./my_connect.php');
+					$mysqli = get_mysqli_conn();
+				?>
 
-			// Prepared statement, stage 2: execute
-			$stmt->execute();
+				<?php
+					// SQL statement
+					$sql = "SELECT c.customerID, c.customerName "
+						. "FROM customer c";
+						
+					// Prepared statement, stage 1: prepare
+					$stmt = $mysqli->prepare($sql);
 
-			// Bind result variables 
-			$stmt->bind_result($customer_id, $customer_name); 
+					// Prepared statement, stage 2: execute
+					$stmt->execute();
 
-			/* fetch values */ 
-			echo '<label for="aid">Pick Customer: </label>'; 
-			echo '<select name="customerID">'; 
-			while ($stmt->fetch()) 
-			{
-			printf ('<option value="%s">%s</option>', $customer_id, $customer_name); 
-			}
-			echo '</select><br>'; 
+					// Bind result variables 
+					$stmt->bind_result($customer_id, $customer_name); 
 
-			/* close statement and connection*/ 
-			$stmt->close(); 
-			$mysqli->close();
-		?>
-			
-
-			<br>
-				<input type="submit" value="Continue"/>
-			</br>
-		</form>
+					/* fetch values */ 
+					echo '<label for="customerID">Pick Customer: </label>'; 
+					echo '<select name="customerID">'; 
+					while ($stmt->fetch()) 
+					{
+						printf ('<option value="%s">%s</option>', $customer_id, $customer_name); 
+					}
+					echo '</select><br>'; 
+					
+					/* close statement and connection*/ 
+					$stmt->close(); 
+					$mysqli->close();
+				?>
+					
+				<br>
+					<input tabindex="4" class="a-button-input" type="submit" value="Sign-in"/>
+				</br>
+				
+			</form>
+		</div>
 	</body>
 </html>
